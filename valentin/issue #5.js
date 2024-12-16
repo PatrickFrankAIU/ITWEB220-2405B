abdc2d46affec6d7dfdae4830e3ed6fde742f541
Valentin Bueno <valentinbuenolarreal@gmail.com>
2024-12-15 20:50:09 -0600

    issue #5 done

---
+++ valentin/issue #5.js
@@ -0,0 +1,5 @@
+let input1 = +prompt('Enter a number here please');
+let input2 = +prompt('Enter another number here please');
+
+remain = input1 % input2;
+console.log("The remainder of " + input1 + ' and ' + input2 + 'is: ' + remain);
