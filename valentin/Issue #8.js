2f88a0120fc69e63727b130f1db56a3f40714279
Valentin Bueno <valentinbuenolarreal@gmail.com>
2024-12-15 21:03:26 -0600

    Positive or Negative number program

---
+++ valentin/Issue #8.js
@@ -0,0 +1,9 @@
+let input = +prompt("enter a number please.");
+
+if(input > 0){
+    console.log("You entered a positive number");
+} else if(input < 0){
+    console.log("You entered a negative number")
+} else{
+    console.log("Your number is zero.")
+}
