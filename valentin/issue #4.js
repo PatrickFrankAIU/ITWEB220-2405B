88b628f2accfa0ac046192c6747735f4243359d5
Valentin Bueno <valentinbuenolarreal@gmail.com>
2024-12-15 20:44:00 -0600

    Issue #4

---
+++ valentin/issue #4.js
@@ -0,0 +1,7 @@
+let input = prompt('type anything here please.')
+
+console.log('First character: ' + input[0]);
+console.log('Second character: ' + input[1]);
+console.log('Third character: ' + input[2]);
+console.log('Fourth character: ' + input[3]);
+console.log('Fifth character: ' + input[4]);
